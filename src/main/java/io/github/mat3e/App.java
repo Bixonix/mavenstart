5bc856123a196ffa0b6624392bf359e50bea3a1e
Dominik <swietlik.dominik@o2.pl>
2023-10-28 15:31:17 +0200

    Initial commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,13 @@
+package io.github.mat3e;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        // System.out.println
+        logger.debug("Hello world");
+
+    }
+}
---
+++ src/test/java/io/github/mat3e/AppTest.java
@@ -0,0 +1,12 @@
+package io.github.mat3e;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testDemo() throws Exception {
+        assertTrue(true);
+    }
+}
